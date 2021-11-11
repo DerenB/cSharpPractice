f1be7bac1912a2e2d53e45671558505823ae6acf
Deren B <dbozerprogramming@gmail.com>
2021-11-10 21:26:06 -0500

    CodeWar Snippets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace cSharpPractice
 {
@@ -8,5 +10,33 @@ namespace cSharpPractice
         {
             Console.WriteLine("Hello World!");
         }
+
+        //Removes all white spaces from string input
+        static string SpacesRemover(string input)
+        {
+            return String.Concat(input.Where(c => !Char.IsWhiteSpace(c)));
+        }
+
+        //Tests a string PIN input
+        //Pin has to be 4 or 6 digits, and only valid numbers (0-9)
+        static bool ValidatePin(string pin)
+        {
+            if (pin.Length == 5 || (pin.Length < 4 || pin.Length > 6))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < pin.Length; i++)
+            {
+                if ((int)pin[i] < 48 || (int)pin[i] > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
+
+
+
