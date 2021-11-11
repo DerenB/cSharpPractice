dee6ad73de779dda36e299089a1ab5c9143b34d0
Deren B <dbozerprogramming@gmail.com>
2021-11-10 21:54:44 -0500

    Reverse Words

--- Program.cs
+++ Program.cs
@@ -35,6 +35,29 @@ namespace cSharpPractice
             }
             return true;
         }
+
+        //Reverses individual words in string input without reversing order of words in string
+        static string ReverseWords(string str)
+        {
+            string[] strSplit = str.Split(' ');
+            string[] reversed = new string[strSplit.Length];
+
+            for (int i = 0; i < strSplit.Length; i++)
+            {
+                char[] wordSplit = strSplit[i].ToCharArray();
+                char[] wordReverse = new char[wordSplit.Length];
+                for (int k = 0; k < wordSplit.Length; k++)
+                {
+                    int endVal = (wordSplit.Length - 1) - k;
+                    wordReverse[endVal] = wordSplit[k];
+                }
+                string reversedWord = new string(wordReverse);
+
+                reversed[i] = reversedWord;
+            }
+            string result = String.Join(" ", reversed);
+            return result;
+        }
     }
 }
 
