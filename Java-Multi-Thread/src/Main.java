746930f9b47c5fd6f36192f130de9f3f6dce575b
Deren <dbozerprogramming@gmail.com>
2022-02-13 19:37:14 -0500

    Setting up the project

--- Java-Multi-Thread/src/Main.java
+++ Java-Multi-Thread/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Scanner keyb = new Scanner(System.in);
+        ArrayList<Integer> inputValues = new ArrayList();
+
+        System.out.println("Enter your list of numbers: ");
+        String input = keyb.nextLine();
+        keyb.close();
+        String[] numbers = input.split(" ");
+
+        for(String item : numbers) {
+            inputValues.add(Integer.parseInt(item));
+        }
+
+        System.out.println();
+        System.out.println("Done");
     }
 }
