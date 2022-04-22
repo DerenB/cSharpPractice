0e2590450b4513d13df128a852b0b562a8ed4776
Deren B <dbozerprogramming@gmail.com>
2022-04-22 01:02:15 -0400

    Name and Base classes

---
+++ Java-Disk-Scheduler/src/LOOK.java
@@ -0,0 +1,10 @@
+/*
+ * DEREN BOZER
+ * COSC-525
+ * MW 1:00PM-2:50PM
+ * PROJECT: Disk Schedulers Assignment
+ */
+
+public class LOOK {
+    // LOOK
+}
--- Java-Disk-Scheduler/src/Main.java
+++ Java-Disk-Scheduler/src/Main.java
@@ -1,5 +1,13 @@
+/*
+ * DEREN BOZER
+ * COSC-525
+ * MW 1:00PM-2:50PM
+ * PROJECT: Disk Schedulers Assignment
+ */
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +15,15 @@ public class Main {
     // Input file name
     private final static String INPUT_FILE_NAME = "input.txt";
 
+    // Input Array List
+    public static ArrayList<Integer> inputList;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Initializes variables
+        inputList = new ArrayList<Integer>();
+
+        // Calls method to read file into arraylist
+        getFileData();
     }
 
     public static void getFileData() {
@@ -19,5 +34,12 @@ public class Main {
             System.out.println("Input File, " + INPUT_FILE_NAME + ", not found.");
             System.exit(1);
         }
+
+        while(fileIn.hasNextInt()) {
+            inputList.add(fileIn.nextInt());
+        }
+
+        // S : the number of milliseconds elapsed since the submission of the previous request
+        // T : the track number requested
     }
 }
