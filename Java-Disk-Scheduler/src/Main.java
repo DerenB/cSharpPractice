6b8d1cad55fd757f25d542da342da71ae67728b4
Deren B <dbozerprogramming@gmail.com>
2022-04-22 00:26:35 -0400

    File input scanner

--- Java-Disk-Scheduler/src/Main.java
+++ Java-Disk-Scheduler/src/Main.java
@@ -1,5 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    // Input file name
+    private final static String INPUT_FILE_NAME = "input.txt";
+
     public static void main(String[] args) {
-        System.out.println("d");
+        System.out.println("Hello World");
+    }
+
+    public static void getFileData() {
+        Scanner fileIn = null;
+        try {
+            fileIn = new Scanner(new FileInputStream(INPUT_FILE_NAME));
+        } catch (FileNotFoundException e) {
+            System.out.println("Input File, " + INPUT_FILE_NAME + ", not found.");
+            System.exit(1);
+        }
     }
 }
