405cf1dde070eab78c036c9c07ae0731bd0053a7
Deren B <dbozerprogramming@gmail.com>
2022-04-23 18:42:29 -0400

    start & end methods

--- Java-Disk-Space-Allocation/src/Main.java
+++ Java-Disk-Space-Allocation/src/Main.java
@@ -6,14 +6,55 @@
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     // Input file name
     private final static String INPUT_FILE_NAME = "input.txt";
 
+    // Input String Array
+    public static ArrayList<String> inputList;
+
+    // Variables
+    public static int numberOfBlocks;
+    public static int numberOfHeadMoves;
+    public static int numberOfFilesNotAllocated;
+    public static boolean fileReadSuccess;
+
     public static void main(String[] args) {
-        System.out.println("d");
+        // Initializes Intput List
+        inputList = new ArrayList<String>();
+
+        // Read input data method
+        getFileData();
+
+        // Initializes variables
+        numberOfBlocks = Integer.parseInt(inputList.get(0));
+        numberOfHeadMoves = 0;
+        numberOfFilesNotAllocated = 0;
+
+        // Start output method
+        StartOutput();
+
+        // End output method
+        EndOutput();
+    }
+
+    private static void StartOutput() {
+        System.out.println("--------------- START CONTIGUOUS SIMULATION ---------------");
+        System.out.println("totBlock = " + numberOfBlocks);
+    }
+
+    private static void EndOutput() {
+        System.out.println("========= Indexed Allocation Statistics =============");
+        System.out.println();
+        System.out.println("During this simulation,");
+        System.out.println("Total head moves = " + numberOfHeadMoves);
+        System.out.println("Total number of files that could not be allocated = " + numberOfFilesNotAllocated);
+        System.out.println();
+        System.out.println("----------------- END of Indexed SIMULATION ---------------");
+        System.out.println();
     }
 
     public static void getFileData() {
@@ -23,6 +64,10 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("Input File, " + INPUT_FILE_NAME + ", not found.");
             System.exit(1);
+        }
+
+        while(fileIn.hasNextLine()) {
+            inputList.add(fileIn.nextLine());
         }
     }
 }
