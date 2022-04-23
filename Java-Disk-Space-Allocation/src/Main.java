e89f0e204a028e6b80d2d45adaa4ce77ac8ac1ad
Deren B <dbozerprogramming@gmail.com>
2022-04-23 18:22:05 -0400

    Project Creation

---
+++ Java-Disk-Space-Allocation/src/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Deren Bozer
+ * COSC 525 MW 1:00pm
+ * Disk Space Allocation
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    // Input file name
+    private final static String INPUT_FILE_NAME = "input.txt";
+
+    public static void main(String[] args) {
+        System.out.println("d");
+    }
+
+    public static void getFileData() {
+        Scanner fileIn = null;
+        try {
+            fileIn = new Scanner(new FileInputStream(INPUT_FILE_NAME));
+        } catch (FileNotFoundException e) {
+            System.out.println("Input File, " + INPUT_FILE_NAME + ", not found.");
+            System.exit(1);
+        }
+    }
+}
